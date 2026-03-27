0a9b9ee062c8f97f28e4fb095f6291ff9856cb83
Vladislav <vladjigalo07@gmail.com>
2026-03-27 23:04:18 +0200

    develop v2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -21,3 +21,13 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(2, 3) == [0, 1, 1]
+
+
+def test_should_return_list_with_length_equal_to_number_of_parts() -> None:
+    result = split_integer(32, 6)
+    assert len(result) == 6
+
+
+def test_difference_between_max_and_min_should_be_less_than_one() -> None:
+    result = split_integer(17, 4)
+    assert max(result) - min(result) <= 1
